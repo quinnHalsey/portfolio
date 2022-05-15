289c2dc65c495857999c452ddbc53f53c3578bf1
Halsey Quinn <halsey.e.quinn@gmail.com>
2022-05-14 20:22:30 -0400

    fix hover over issue

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ function App() {
   return (
     <div className="page-container">
       <div className="centered-container white">
-        <div
-          onMouseEnter={() => setExpand(true)}
-          onMouseLeave={() => setExpand(false)}
-        >
-          Halsey Quinn
+        <div>
+          <span
+            onMouseEnter={() => setExpand(true)}
+            onMouseLeave={() => setExpand(false)}
+          >
+            Halsey Quinn
+          </span>
         </div>
         <div className="icon-links">
           <a href="https://github.com/quinnHalsey" className="icon-link">
